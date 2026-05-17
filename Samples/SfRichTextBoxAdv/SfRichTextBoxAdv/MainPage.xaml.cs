cb5bc464883ab66266447072df0db6a9e0de3492
Kalaivannan-Ganesan <93248069+Kalaivannan-Ganesan@users.noreply.github.com>
2026-05-17 16:59:52 +0530

    Added Basic sfrichTextBoxadv sample for UWP

---
+++ Samples/SfRichTextBoxAdv/SfRichTextBoxAdv/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Syncfusion.UI.Xaml.RichTextBoxAdv;
+using System.IO;
+using System.Reflection;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace SfRichTextBoxAdv
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        #region Constructor
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            //Loads the document into UWP SfRichTextBoxAdv.
+            Stream fileStream = typeof(MainPage).GetTypeInfo().Assembly.GetManifestResourceStream("SfRichTextBoxAdv.Assets.GettingStarted.docx");
+            richTextBoxAdv.Load(fileStream, FormatType.Docx);
+        }
+        #endregion
+    }
+}
